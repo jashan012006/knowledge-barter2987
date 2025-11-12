9e77c9497667452d9f92a144fbba62ed23b87776
jashan012006 <jashan0229.becse24@chitkara.edu.in>
2025-11-12 15:44:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PostSkill from "./PostSkill";
 import BrowseSkill from "./BrowseSkill";
 import ViewSkill from "./ViewSkill";
 import AboutUs from "./AboutUs"
+import Contact from "./Contact"
 
 
 
@@ -20,8 +21,8 @@ const App = () => {
       <Route path="/post-skill" element={<PostSkill />} />
       <Route path="/browse-skill" element={<BrowseSkill/>}/>
       <Route path="/view/:id" element={<ViewSkill/>}/>
-      <Route path="/chat/:chatId" element={<Chat />} />
       <Route path="/aboutus/" element={<AboutUs/>}/>
+      <Route path="/contact/" element={<Contact/>}/>
 
 
       
