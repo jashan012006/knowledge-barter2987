aeac7b24e81ffd872757261544d7cf58438f3df4
jashan012006 <jashan0229.becse24@chitkara.edu.in>
2025-11-12 15:38:32 +0530

    Add Chat and AboutUs routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,28 @@ import Home from "./Home";
 import Login from "./login";
 import Signup from "./Signup";
 import PostSkill from "./PostSkill"; 
-import BrowseSkill from "./BrowseSkill"
-import ViewSkill from "./ViewSkill"
+import BrowseSkill from "./BrowseSkill";
+import ViewSkill from "./ViewSkill";
+import Chat from "./chat";
+import AboutUs from "./AboutUs"
+
+
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
       <Route path="/post-skill" element={<PostSkill />} />
       <Route path="/browse-skill" element={<BrowseSkill/>}/>
       <Route path="/view/:id" element={<ViewSkill/>}/>
+      <Route path="/chat/:chatId" element={<Chat />} />
+      <Route path="/aboutus/" element={<AboutUs/>}/>
+
+
+      
 
     </Routes>
   );
